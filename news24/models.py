f846e7ef3f215540b828688bfe1e6ab395e05b09
unknown <kajalk965441@gmail.com>
2023-05-27 21:28:53 +0530

    push my code

---
+++ news24/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class contact(models.Model):
+   name= models.CharField(max_length=100, null=True, blank=True) 
+   email= models.EmailField(null=True, blank=True) 
+   subject= models.CharField(max_length=100, null=True, blank=True) 
+   meassage= models.TextField(null=True, blank=True) 
+   
+def __str__(self):
+        return self.name
