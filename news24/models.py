a536f29087a3b3ade18b3fcdea6cc7c39da69fd2
unknown <kajalk965441@gmail.com>
2023-05-28 20:52:21 +0530

    nnn

--- news24/models.py
+++ news24/models.py
@@ -10,13 +10,16 @@ class contact(models.Model):
    def __str__(self):
       return self.name
 
-martial_status = [
+category = [
 				('Tech', 'Tech'), 
 				('Sports', 'Sports'), 
 				('Fashion', 'Fashion'), 
+            ('Business', 'Business'), 
+            ('Entertainment', 'Entertainment'),
+            ('Latest_News', 'Latest_News'), 
 				]
 class news(models.Model):
-   category = models.CharField(max_length=100, choices=martial_status,    null=True, blank=True)
+   category = models.CharField(max_length=100, choices=category,    null=True, blank=True)
    title= models.CharField(max_length=100, null=True, blank=True) 
    details= models.TextField(null=True, blank=True) 
    time= models.DateTimeField(default = timezone.now, null=True, blank=True) 
